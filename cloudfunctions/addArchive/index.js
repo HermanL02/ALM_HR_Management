d74cfcf7d4e6e68dbc563077b9d24c694e21d2c4
Yiqun Liang <Hermanyiqunliang@gmail.com>
2023-05-30 05:56:04 -0300

    First commit
    鉴权未补充，正在编写获取数据的微后端

--- cloudfunctions/addArchive/index.js
+++ cloudfunctions/addArchive/index.js
@@ -1,9 +1,6 @@
 // 云函数入口文件
 const cloud = require('wx-server-sdk');
-const { eventBridge } = require('XrFrame/kanata/lib/index');
-
 cloud.init({ env: cloud.DYNAMIC_CURRENT_ENV }) // 使用当前云环境
-
 const db = cloud.database();
 
 // 云函数入口函数
