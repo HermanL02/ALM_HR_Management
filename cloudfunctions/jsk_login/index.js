dda80122dd394417fcac8cc428e5a111f378ffa6
HermanL02 <Hermanyiqunliang@gmail.com>
2023-08-08 11:23:46 +0800

    推送
    推送

---
+++ cloudfunctions/jsk_login/index.js
@@ -0,0 +1,26 @@
+// 云函数入口文件
+const cloud = require('wx-server-sdk')
+cloud.init({ env: cloud.DYNAMIC_CURRENT_ENV }) // 使用当前云环境
+
+// 云函数入口函数
+// index.js
+
+exports.main = async (event, context) => {
+  // 这里获取到的 openId、 appId 和 unionId 是可信的，注意 unionId 仅在满足 unionId 获取条件时返回
+  let { FROM_OPENID, FROM_APPID, FROM_UNIONID } = cloud.getWXContext()
+  const db = cloud.database()
+  const admins = await db.collection('admin').where({
+    _openid: FROM_OPENID
+  }).get()
+  let ROLE = 'user';
+  if(admins.data.length > 0){
+    ROLE = admins.data[0].role || 'user'; // 从数据库中读取role，如果不存在则默认为'user'
+  }
+  return {
+    FROM_OPENID,
+    FROM_APPID,
+    FROM_UNIONID,
+    ROLE,
+  }
+}
+
